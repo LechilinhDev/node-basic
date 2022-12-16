c2ed8f9905d1e28b209b4cf4b8655da2d09839f1
Le Chi Linh Dev <dothihue2689@gmail.com>
2022-12-16 23:02:35 +0700

    setup route and controller

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import configViewEngine from './config/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// setup view engine ejs
 configViewEngine(app);
+// init Route
+initWebRoute(app);
 
-app.get('/', (req, res) => {
-    res.render("index.ejs");
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);

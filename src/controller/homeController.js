b1606373cee1686f5eb45969d4f9702f76f1e0f8
Le Chi Linh Dev <dothihue2689@gmail.com>
2022-12-27 02:33:06 +0700

    upload multiple File and Singel File

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,6 @@
 import pool from '../config/connectDB';
+import multer from 'multer';
+// import path from 'path';
 let getHomePage = async (req, res) => {
 
     const [rows, fields] = await pool.execute('SELECT * FROM `users`');
@@ -40,6 +42,80 @@ let updateUser = async (req, res) => {
     return res.redirect('/');
 }
 
+let uploadFile = (req, res) => {
+
+    return res.render('uploadFile.ejs');
+}
+
+
+
+
+
+
+
+
+let handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    const upload = multer().single('profile_pic');
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="image/${req.file.filename}" width="500"><hr /><a href="./uploadFile">Upload another image</a>`);
+    });
+
+
+
+}
+
+let handleUploadMultipleFile = async (req, res) => {
+    // logic upload multiple controller
+
+
+    if (req.fileValidationError) {
+        return res.send(req.fileValidationError);
+    }
+    else if (req.files.length === 0) {
+
+        return res.send('Please select an image to upload');
+    }
+
+    else {
+        let result = "You have uploaded these images: <hr />";
+        const files = req.files;
+        let index, len;
+
+        // Loop through all the uploaded images and display them on frontend
+        for (index = 0, len = files.length; index < len; ++index) {
+
+            result += `<img src="image/${files[index].filename}" width="300" style="margin-right: 20px;">`;
+        }
+        result += '<hr/><a href="./uploadFile">Upload more images</a>';
+        res.send(result);
+
+    }
+
+
+
+
+
+}
+
 module.exports = {
-    getHomePage, getDetailPage, createNewUser, deleteUser, editUser, updateUser
+    getHomePage, getDetailPage, createNewUser, deleteUser, editUser, updateUser, uploadFile, handleUploadFile, handleUploadMultipleFile
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,34 @@
 import express, { Router } from 'express';
 import homeController from '../controller/homeController';
+import multer from 'multer';
+import path from 'path';
+var approot = require('app-root-path');
 let router = express.Router();
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        cb(null, approot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+let uploadMultipleFile = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images', 3);
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
@@ -10,6 +38,24 @@ const initWebRoute = (app) => {
     router.post('/deleteUser', homeController.deleteUser);
     router.get('/editUser/:id', homeController.editUser);
     router.post('/updateUser', homeController.updateUser);
+    router.get('/uploadFile', homeController.uploadFile);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+    router.post('/upload-multiple-images', (req, res, next) => {
+        uploadMultipleFile(req, res, (err) => {
+            if (err instanceof multer.MulterError) {
+                return res.send('Vượt quá giới hạn !')
+            } else if (req.fileValidationError) {
+                return res.send(req.fileValidationError);
+            } else if (err) {
+                return res.send('Lỗi không xác định!')
+            }
+            else {
+                next();
+            }
+
+
+        })
+    }, homeController.handleUploadMultipleFile)
     return app.use('/', router);
 }
 

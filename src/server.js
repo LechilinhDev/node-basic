2cc7f3913621e169e8bdd7dcb6dfc643e648517e
Le Chi Linh Dev <dothihue2689@gmail.com>
2022-12-27 11:14:48 +0700

    middleware404 vs morgan logging

--- src/server.js
+++ src/server.js
@@ -4,14 +4,18 @@ import initWebRoute from './route/web';
 import initAPIRoute from './route/api';
 // import connectDB from './config/connectDB';
 require('dotenv').config();
+var morgan = require('morgan')
 const app = express();
 const port = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(morgan('combined'))
 // setup view engine ejs
 configViewEngine(app);
+
+
 // init Route
 initWebRoute(app);
 
@@ -19,6 +23,14 @@ initWebRoute(app);
 initAPIRoute(app);
 
 
+// midedleware 404 erro!
+app.use((req, res, next) => {
+
+    if (res.status(404).statusCode === 404) {
+        return res.render('404.ejs');
+    };
+    next();
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
